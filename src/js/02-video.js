3948a233b811e1aeabd9d1f847eb60608c8aafb8
VH91 <hritsik.v.1991@gmail.com>
2022-05-21 00:19:24 +0300

    finished video.js/html

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import throttle from 'lodash/throttle'
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+console.log(player);
+
+window.onload = () =>
+{
+    let isTime = localStorage.getItem("videoplayer-current-time");
+    if (isTime)
+    {
+        let time = JSON.parse(isTime).seconds;
+        player.setCurrentTime(time);
+        //console.log(time);
+    }
+}
+
+player.on('timeupdate', throttle((data) => {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(data))
+}, 1000));
