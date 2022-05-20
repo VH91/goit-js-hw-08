5d4bc8c5cbd5b5ac13b33bc5a5001a6fd4a6344e
VH91 <hritsik.v.1991@gmail.com>
2022-05-20 23:32:48 +0300

    finished gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const galleryImg = makeGalery(galleryItems);
+
+galleryEl.innerHTML = galleryImg;
+
+openLightBox();
+
+function makeGalery(items) {
+    return items.map(({original, preview, description}) => `   
+        <a class="gallery__item" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"            
+            alt="${description}"            
+            />
+        </a>    
+    `).join("");
+}
+
+function openLightBox() {
+  new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionsData: 'alt',
+    close: true,   
+});   
+}   
+
